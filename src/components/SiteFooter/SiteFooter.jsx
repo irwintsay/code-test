b2e8232931631ef1e7f01552f7b0098a5759ac1f
Irwin Tsay <irwintsay@gmail.com>
2017-03-16 16:28:46 -0400

    Update footer with links, Update carousel content

--- src/components/SiteFooter/SiteFooter.jsx
+++ src/components/SiteFooter/SiteFooter.jsx
@@ -5,9 +5,9 @@ const SiteFooter = props => (
   <footer className="SiteFooter">
     <ul>
       <li className="footer-item">&copy; 2017 &mdash; SPARTA PLAESENT</li>
-      <li className="footer-item"> - INSTAGRAM</li>
-      <li className="footer-item"> - FACEBOOK</li>
-      <li className="footer-item"> - TWITTER</li>
+      <li className="footer-item"> - <a href="instagram.com">INSTAGRAM</a></li>
+      <li className="footer-item"> - <a href="facebook.com">FACEBOOK</a></li>
+      <li className="footer-item"> - <a href="twitter.com">TWITTER</a></li>
     </ul>
   </footer>
 );
